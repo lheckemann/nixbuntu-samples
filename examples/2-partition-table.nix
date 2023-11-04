bc5f676633e99eb713e2635d61afd6a95c82f0a2
Linus Heckemann <git@sphalerite.org>
2023-11-04 11:15:29 +0100

    working grub efi

--- examples/2-partition-table.nix
+++ examples/2-partition-table.nix
@@ -3,18 +3,18 @@ vmTools.makeImageFromDebDist {
   inherit (vmTools.debDistros.ubuntu2004x86_64) name fullName urlPrefix packagesLists;
 
   packages = lib.filter (p: !lib.elem p [
-      "g++" "make" "dpkg-dev" "pkg-config"
-      "sysvinit"
+    "g++" "make" "dpkg-dev" "pkg-config"
+    "sysvinit"
   ]) vmTools.debDistros.ubuntu2004x86_64.packages ++ [
     "systemd" # init system
     "init-system-helpers" # ???
     "systemd-sysv" # ???
     "dbus" # ???
     "linux-image-generic" # kernel
-    "linux-image-5.4.0-26-generic"
+    #"linux-image-5.4.0-26-generic"
     "initramfs-tools" # hooks for generating an initramfs
     "e2fsprogs" # for fsck
-    #"sicherboot"
+    "grub-efi"
   ];
 
   size = 8192;
@@ -34,15 +34,6 @@ vmTools.makeImageFromDebDist {
     mkdir -p /mnt/{proc,dev,sys,boot/efi}
     ${util-linux}/bin/mount -t vfat "$disk"1 /mnt/boot/efi
     touch /mnt/.debug
-
-    mkdir -p /mnt/etc/kernel/postinst.d
-    cat >>/mnt/etc/kernel/postinst.d/zz-install-kernel <<EOF
-    #!/bin/sh
-    set -x
-    exec kernel-install add "$1" "$2"
-    EOF
-    chmod a+x /mnt/etc/kernel/postinst.d/zz-install-kernel
-
   '';
 
   postInstall = ''
@@ -53,17 +44,10 @@ vmTools.makeImageFromDebDist {
     ${udev}/bin/udevadm trigger
     ${udev}/bin/udevadm settle
 
-    mkdir -p /mnt/nix
-    ${util-linux}/bin/mount --rbind /nix /mnt/nix
-    ${util-linux}/bin/mount -t devtmpfs devtmpfs /mnt/dev
     ${util-linux}/bin/mount -t sysfs sysfs /mnt/sys
 
     chroot /mnt /bin/bash -exuo pipefail <<CHROOT
     export PATH=/usr/sbin:/usr/bin:/sbin:/bin
-
-    #SYSTEMD_IGNORE_CHROOT=1 /lib/systemd/systemd-udevd &
-    #SYSTEMD_IGNORE_CHROOT=1 udevadm trigger
-    #SYSTEMD_IGNORE_CHROOT=1 udevadm settle
 
     # update-initramfs needs to know where its root filesystem lives,
     # so that the initial userspace is capable of finding and mounting it.
@@ -72,18 +56,23 @@ vmTools.makeImageFromDebDist {
     dpkg --configure -a
 
     # actually generate an initramfs
-    /usr/sbin/update-initramfs -k all -c
+    update-initramfs -k all -c
 
     # Install the boot loader to the EFI System Partition
-    bootctl install
+    cat >> /etc/default/grub <<EOF
+    GRUB_TIMEOUT=5
+    GRUB_CMDLINE_LINUX=""
+    GRUB_CMDLINE_LINUX_DEFAULT=""
+    EOF
+    sed -i '/TIMEOUT_HIDDEN/d' /etc/default/grub
+    update-grub
+    grub-install --target x86_64-efi
 
     # Set a password so we can log into the booted system
     echo root:root | chpasswd
 
     CHROOT
     ${util-linux}/bin/umount /mnt/boot/efi
-    ${util-linux}/bin/umount /mnt/dev
     ${util-linux}/bin/umount /mnt/sys
-    ${util-linux}/bin/umount -R /mnt/nix
   '';
 }
